23a0972f81528ec842bc62f3d3169cafac6ec5a8
Mark Kuo <starryalley@gmail.com>
2025-03-12 10:32:02 +1100

    abs: change to use "Add" instead of "Replace"
    This allows multiple lua to modulate throttle altogether.

--- mixes/abs.lua
+++ mixes/abs.lua
@@ -8,16 +8,17 @@ ABS (anti-lock braking system) for cars.
 
 INPUT:
   Thr: SOURCE, should be set to the throttle channel
-  Brake: SOURCE, the braking strength in percentage (-100% to 0%). Assign a global variable (set to %) to this
   CycleT: SOURCE, ABS cycle duration (ms). Must be > 10ms. Assign a global variable to this
   DutyC: SOURCE, ABS duty cycle (% of cycle time braking) (0%-100%)
+  BrkTh: VALUE, brake threshold to activate ABS, default -30
+  OnTh: VALUE, ON phase throttle value (-100 to 0), default is 0 which means current throttle. Otherwise set throttle to this value
+  OffTh: VALUE, OFF phase throttle value (-100 to 0), defualt is -10. Note that setting this to 0 may cause the car to activate reverse instead of brake.
 
 OUTPUT:
   Brk: this should be "add"ed to the throttle mixer so it applies the modulated brake (throttle value)
 
-
 Usage in Mix:
-  Multiplex must be set to "Replace" (with Curve Func x<0)
+  Multiplex must be set to "Add".
 
 --]]
 
@@ -25,15 +26,14 @@ local input = {
     {"Thr", SOURCE},
     {"CycleT", SOURCE},
     {"DutyC", SOURCE},
-    {"BrkTh", VALUE, -100, -10, -30}, -- brake threshold to activate ABS, default -30
-    {"OnTh", VALUE, -100, 0, 0}, -- ON phase throttle value (-100 to 0), default is the special value 0, which means current throttle value
-    {"OffTh", VALUE, -100, 0, 0}, -- OFF phase throttle value (-100 to 0), default is no brake (0)
+    {"BrkTh", VALUE, -100, -10, -30},
+    {"OnTh", VALUE, -100, 0, 0},
+    {"OffTh", VALUE, -100, 0, -10},
 }
 
 local output = {"Brk"}
 
 
-local previous_throttle = 0
 local abs_start_time = 0
 local abs_active = false
 
@@ -68,18 +68,19 @@ local function run(throttle, abs_cycle_time, abs_duty_cycle, brake_threshold, on
         -- Apply brake if within ON phase of ABS cycle
         if cycle_elapsed < abs_on_time then
             if on_throttle == 0 then
-                return throttle -- ON phase: use the current throttle value
+                return 0 -- ON phase: use the current throttle value
             else
-                return on_throttle*factor -- ON phase: use the predefined throttle value
+                return on_throttle*factor-throttle -- ON phase: use the predefined throttle value
             end
         else
-            return off_throttle*factor  -- Release brake during OFF phase
+            return off_throttle*factor-throttle  -- Release brake during OFF phase
         end
     else
         abs_active = false  -- Reset ABS when throttle is released
     end
 
-    return throttle  -- Default: pass-through throttle
+    -- no change to brake
+    return 0
 end
 
 return { input = input, output = output, run = run }
